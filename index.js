f8c23ff71b91885c65af74445baf765fae684c55
Vardhan Gollapalli <vardhangollapalli87@gmail.com>
2026-03-02 23:51:33 +0530

    intital

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 
 
 app.get('/',(req,res)=>{
-    res.send("<h1>HELLO FROM A_Z AUTOMATION<h1>",contentType="text/html");
+    res.send("<h1>HELLO FROM A_Z AUTOMATION</h1>",contentType="text/html");
 })
 
 app.listen(3020,()=>{
