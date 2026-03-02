309be394a513bc52aee9dea2c8e10713bd96678c
Vardhan Gollapalli <vardhangollapalli87@gmail.com>
2026-03-02 23:52:49 +0530

    second

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-const express = require('express')
-const app = express()
+// const express = require('express')
+// const app = express()
 
 
-app.get('/',(req,res)=>{
-    res.send("<h1>HELLO FROM A_Z AUTOMATION</h1>",contentType="text/html");
-})
+// app.get('/',(req,res)=>{
+//     res.send("<h1>HELLO FROM A_Z AUTOMATION</h1>",contentType="text/html");
+// })
 
-app.listen(3020,()=>{
-    console.log(`server is running on http://localhost:3020`);
-})
+// app.listen(3020,()=>{
+//     console.log(`server is running on http://localhost:3020`);
+// })
+
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3020;
+
+app.get('/', (req, res) => {
+    res.type('html');
+    res.send("<h1>HELLO FROM A_Z AUTOMATION</h1>");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
