c68081ce330d08b15160eb27fdab8c613573e6ae
Vardhan Gollapalli <vardhangollapalli87@gmail.com>
2026-03-02 23:53:42 +0530

    second

--- index.js
+++ index.js
@@ -13,7 +13,7 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 3020;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.type('html');
